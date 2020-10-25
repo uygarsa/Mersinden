5867e63f0f7061abecd1cc5b8a86de799e93e96d
uygar sarıoğlu <uygarsarioglu@gmail.com>
2020-10-25 11:19:04 +0300

    Add project files.

---
+++ MersindenWeb.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MersindenWeb.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
